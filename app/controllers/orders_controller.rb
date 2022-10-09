9d05bc7acb091a48cfef0ca9fb2ee4cabb35af9a
Dhaval G <gandhi.dhaval038@gmail.com>
2022-10-09 19:19:33 +0530

    - Implement Order create and update

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,6 +9,35 @@ class OrdersController < ApplicationController
   def new
     @customers = Customer.all
     @items = Item.all
+    @order = Order.new
+  end
+
+  def create
+    @order = Order.new(order_params)
+    if @order.save
+      flash[:notice] = 'Order Successfully Created!'
+      redirect_to orders_path
+    else
+      flash[:alert] = @order.errors.full_messages.join(', ')
+      render :new
+    end
+  end
+
+  def edit
+    @customers = Customer.all
+    @items = Item.all
+    @order = Order.find_by_id(params[:id])
+  end
+
+  def update
+    @order = Order.find_by_id(params[:id])
+    if @order.update(order_params)
+      flash[:notice] = 'Order Successfully Updated!'
+      redirect_to orders_path
+    else
+      flash[:alert] = @order.errors.full_messages.join(', ')
+      render :edit
+    end
   end
 
   def customer_search
@@ -21,6 +50,12 @@ class OrdersController < ApplicationController
     @search = params[:search]
     @items = Item.all
     @items = @items.where('name ilike :search', search: "%#{@search}%") if @search.present?
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:customer_id, :date, order_items_attributes: [:id, :item_id, :qty, :rate, :_destroy])
   end
 
 end
