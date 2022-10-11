0ccaf2269f17c5dda18113c3bab57d850c595f18
D. Gandhi <gandhi.dhaval038@gmail.com>
2022-10-11 12:04:48 +0530

    - Uncommnet authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,3 @@
 class ApplicationController < ActionController::Base
-  # http_basic_authenticate_with :name => "admin", :password => "v.negandhi"
+  http_basic_authenticate_with :name => "admin", :password => "v.negandhi"
 end
